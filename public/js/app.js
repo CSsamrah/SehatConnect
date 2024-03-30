40c49e133da276eee0bb75ca419e163afe79de99
CSSamrah <samra.fatima.790@gmail.com>
2024-03-30 17:53:23 +0500

    update

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let loginButton=document.getElementById('loginButton')
+    loginButton.addEventListener('click',function(){
+        window.location.href="./pages/login.html";
+
+})
+})
+
+    
+
+
+    
